8d713962fa109f7eada328bbb30c1289d38567e1
Michael <engstiven@gmail.com>
2020-02-08 18:16:24 +0100

    Algoritmo de Insercion

---
+++ DesignAlgorithms/src/Algoritmos/Insercion.java
@@ -0,0 +1,37 @@
+package Algoritmos;
+
+public class Insercion {
+	
+	public Insercion() {}
+	
+	public static int[] algortimoInsercion(int[] Array) 
+	{
+		int j = 0;
+		int x = 0;
+		
+		if( Array.length > 0 )
+		{
+			for (int i = 2; i < Array.length; i++) 
+			{
+				x = Array[i];
+				j = i -1;
+				
+				while ( j > 0 && x < Array[j] ) 
+				{
+					Array[j+1] = Array[j];
+					j = j- 1;
+				}
+				Array[j+1] = x;
+			}
+			
+			System.out.println("Exito en la operacion");
+			return Array;
+		}
+		else
+		{
+			System.out.println("Error, el array en invalido");
+			return Array;
+		}
+		
+	}
+}
