39513b3e552cb0d51b856b264cdce172a4e72599
Maicol_Pulido <engstiven@gmail.com>
2020-05-25 17:54:06 -0500

    Start

--- Test/BiciTurbo-App/src/app/user.service.ts
+++ Test/BiciTurbo-App/src/app/user.service.ts
@@ -5,5 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:7598/user';
+  constructor( private http: HttpClient ) { }
 }
---
+++ Test/BiciTurbo-App/src/app/user.ts
@@ -0,0 +1,11 @@
+export class User{
+  id:number;
+  nombre:string;
+  ciudad:string;
+  password:string;
+  telefono:string;
+  email:string;
+  fecha_creacion:string;
+  fecha_modificacion:string;
+  usuario_creacion:string;
+}
