0517261ae312ff61cb13632b049f2f17953f0950
Maicol_Pulido <engstiven@gmail.com>
2020-05-25 17:22:04 -0500

    Test Node y Angular

--- Test/BiciTurbo-App/karma.conf.js
+++ Test/BiciTurbo-App/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/prueba-app'),
+      dir: require('path').join(__dirname, './coverage/BiciTurbo-App'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- Test/BiciTurbo-App/src/app/app.module.ts
+++ Test/BiciTurbo-App/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
